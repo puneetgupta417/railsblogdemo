afd96631d817d5a2922d6e3e5888cb2eb0af9ba1
Puneet Gupta <puneetgupta417@gmail.com>
2016-03-03 23:10:50 +0530

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -36,8 +36,6 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  root 'application#hello'
-
   # Example resource route with options:
   #   resources :products do
   #     member do
